f5ce864cd9e61dd7c61d6bde2563d6bacbbd47af
Bing5154 <bli7@stuy.edu>
2018-02-08 19:07:23 -0500

    generate csv file code

--- MatrixFinder.java
+++ MatrixFinder.java
@@ -27,6 +27,11 @@
  *       in which case we return (-1, -1)
  */
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+
 public class MatrixFinder {
     public static String search(int[][] matrix, int target){
 	int col = matrix.length - 1; //starting at upper right corner
@@ -56,6 +61,55 @@ public class MatrixFinder {
     }
     
     public static void main(String[] args){
+        PrintWriter pw = null;
+	try {
+	    pw = new PrintWriter(new File("data.csv"));
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
+	}
+
+	StringBuilder sb = new StringBuilder();
+	
+	//column label of csv files
+	sb.append("size ");
+	sb.append(", ");
+	sb.append("type ");
+	sb.append(", ");
+	sb.append("Trial 1");
+	sb.append(", ");
+	sb.append("Trial 2");
+	sb.append(", ");
+	sb.append("Trial 3");
+	sb.append(", ");
+	sb.append("Trial 4");
+	sb.append(", ");
+	sb.append("Trial 5");
+	sb.append(", ");
+      	sb.append("Trial 6");
+	sb.append(", ");
+	sb.append("Trial 7");
+	sb.append(", ");
+	sb.append("Trial 8");
+	sb.append(", ");
+	sb.append("Trial 9");
+	sb.append(", ");
+	sb.append("Trial 10");
+	sb.append(", ");
+	sb.append("Trial 11");
+	sb.append(", ");
+	sb.append("Trial 12");
+	sb.append(", ");
+	sb.append("Trial 13");
+	sb.append(", ");
+	sb.append("Trial 14");
+	sb.append(", ");
+	sb.append("Trial 15");
+	sb.append(", ");
+	sb.append("average of 1000 trials");
+	sb.append(", ");
+	sb.append("\n");
+
+
 	int arrs = 1;
 	
 	//testing arrs starting at 2000x2000 and then increasing the length by 2000
@@ -67,35 +121,53 @@ public class MatrixFinder {
 		
 	    System.out.println("\n------------------For a " + arrs + "000x" + arrs + "000 array-----------------------");  //deleted for CSV
 	    //testing out runtime
-	    System.out.println("\nRuntime for worst case:  (only shown the first 10 trials) \n");
+	    System.out.println("\nRuntime for worst case:  (only shown the first 15 trials) \n");
+	    //for the csv
+	    sb.append( arrs + "000" + ", ");
+	    sb.append("worst, ");
+
 	    while(trial <= 1000){
 		long startTime = System.nanoTime();
 		search(bigtest, bigtest[bigtest.length-1][0]); //code being tested 
 		long estimatedTime = System.nanoTime() - startTime;
 		if(trial <= 15){
-		    System.out.println("trial " + trial + "," + estimatedTime + " nanoseconds.");
+		    System.out.println("trial " + trial + ", " + estimatedTime + " nanoseconds.");
+		    sb.append(estimatedTime);
+		    sb.append(", ");
 		}
 		trial++;
 		ave += estimatedTime;
 	    }
-	    System.out.println("Average,"+ (ave/1000) + " nanoseconds.");
+	    sb.append(ave/1000);
+	    sb.append('\n');
+	    System.out.println("Average of 1000 trials: "+ (ave/1000) + " nanoseconds.");
 
 	    trial = 1;
 	    ave = 0;
-	    System.out.println("\nRuntime for best case:  (only shown the first 10 trials) \n");
+	    System.out.println("\nRuntime for best case:  (only shown the first 15 trials) \n");
+	    //for csv
+	    sb.append( arrs + "000" + ", ");
+	    sb.append("best, ");
+	    
 	    while(trial <= 1000){
 		long startTime = System.nanoTime();
 		search(bigtest, bigtest[0][bigtest.length-1]); //code being tested 
 		long estimatedTime = System.nanoTime() - startTime;
 		if(trial <= 15){
-		    System.out.println("trial " + trial + "," + estimatedTime +" nanoseconds.");
+		    System.out.println("trial " + trial + ", " + estimatedTime +" nanoseconds.");
+		    sb.append(estimatedTime);
+		    sb.append(", ");
 		}
 		trial++;
 		ave += estimatedTime;
 	    }
-	    System.out.println("Average,"+ (ave/1000) + " nanoseconds.");
+	    System.out.println("Average of 1000 trials: "+ (ave/1000) + " nanoseconds.");
+	    sb.append(ave/100);
+	    sb.append('\n');
 	    arrs += 1;
-	}	    				     	
+	}
+	pw.write(sb.toString());
+	pw.close();
     }//end of main	    
 }
 
